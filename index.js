986a18b9401ab087f0407ec7c61be386893d8170
Shariq <shariqnadeem21@hotmail.com>
2023-03-04 21:35:42 +0800

    Created basic UI for rock paper scissors game as well as added css styling and logic for game as required

--- index.js
+++ index.js
@@ -1,5 +1,64 @@
+//declare all html tags for buttons and div
+let playerScore = 0;
+let computerScore = 0;
+
+let buttons = document.querySelectorAll(".button");
+
+const body = document.querySelector("body");
+const container = document.querySelector('#container');
+const message_cont = document.getElementById('message-container');
+
+//div to display results 
+const end_div = document.getElementById('end-div')
+const endDisplay = document.getElementById('endDisplay')
+
+const playerSDisplay = document.getElementById('playerSDisplay');
+playerSDisplay.textContent = 0;
+// scoreDisplay.appendChild(playerSDisplay);
+
+const computerSDisplay = document.getElementById('computerSDisplay')
+computerSDisplay.textContent = 0;
+
+
+//display live updates 
+const message = document.querySelector('#message');
+
+
+
+//buttons for selection
+const rock_btn = document.querySelector('#btn-rock');
+rock_btn.addEventListener('click', ()=>{
+    if (playerScore !=5  && computerScore != 5){
+        round = playRound("rock", getComputerChoice());
+        game(round);
+    }
+});
+const paper_btn = document.querySelector('#btn-paper');
+paper_btn.addEventListener('click', ()=>{
+    if (playerScore !=5 && computerScore != 5){
+        round = playRound("paper", getComputerChoice());
+        game(round);
+    }
+});
+const scissors_btn = document.querySelector('#btn-scissors');
+scissors_btn.addEventListener('click', ()=>{
+    if (playerScore !=5 && computerScore != 5){
+        round = playRound("scissors", getComputerChoice());
+        game(round);
+    }
+});
+
+//button for reset 
+const reset_btn = document.querySelector('#reset');
+reset_btn.addEventListener('click', ()=> {
+    reset();
+})
+
+
+
+
 function getComputerChoice() {
-    let choices = ["Rock", "Paper", "Scissors"];
+    let choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
@@ -7,71 +66,101 @@ function playRound(playerSelection, computerSelection){
     const selections = ["rock", "paper", "scissors"];
     var result = ""
 
-    let pselect = playerSelection.toLowerCase();
-    let cselect = computerSelection.toLowerCase();
-    
-    // console.log(pselect);
-    // console.log(cselect);
+    // console.log(`The player selected: ${playerSelection}`);
+    // console.log(`The pc selected: ${computerSelection}`);
+
+    //group together pc losses and player losses
 
     //draw
-    if (pselect == cselect){   
-        result = ["This is a draw", 2];
-    }
-    //rock and paper
-    else if (pselect == selections[0] && cselect == selections[1]){
-        result = ["You Lose ! Paper beats Rock", 2];
-    }
-    else if (pselect == selections[1] && cselect == selections[0]){
-        result = ["You Win ! Paper beats Rock", 1];
-    }
-    //rock and scissors
-    else if (pselect == selections[0] && cselect == selections[2]){
-        result = ["You Win ! Rock beats Scissors", 1];
-    }
-    else if (pselect == selections[2] && cselect == selections[0]){
-        result = ["You Lose ! Rock beats Scissors", 2];
-    }
-    //paper and scissors
-    else if (pselect == selections[1] && cselect == selections[2]){
-        result = ["You Lose ! Scissors beats Paper", 2];
-    }
-    else if (pselect == selections[2] && cselect == selections[1]){
-        result = ["You Win ! Scissors beats Paper", 1];
-    }
-    return result;
-
-}
-
-function game() {
-    var pscore = 0;
-    var cscore = 0;
-
-    for (let i = 0; i < 5; i++){
-        let user = prompt("Enter your choice: ");
-        const computerSelection = getComputerChoice();
-        let round = playRound(user, computerSelection);
-        let roundMessage = round[0];
-        console.log(roundMessage);
-        let roundScore = round[1];
+    if (playerSelection==computerSelection){
+        result = "Draw";
+        // console.log(playerSelection);
+        message.textContent = `${playerSelection} does not beat ${computerSelection} !`
         
 
-        if (roundScore == 1){
-            pscore ++;
-        }
-        else if (roundScore==2){
-            cscore ++;
-        }
     }
+    else if (playerSelection == selections[0] && computerSelection == selections[1] || 
+        playerSelection == selections[2] && computerSelection == selections[0] || playerSelection == selections[1] && computerSelection == selections[2]) {
+            result = "Computer";
+            console.log("Computer wins");
 
-    if (pscore > cscore){
-        console.log("User wins !");
+            message.textContent = `${playerSelection} defeated by ${computerSelection} !`
+          
     }
     else {
-        console.log("Computer wins !");
+        result = "User"
+        console.log("User wins");
+        message.textContent = `${playerSelection} defeats ${computerSelection} !`
+       
     }
+
+    return result;
 }
 
-// const playerSelection = "rock";
+function game(round) {
+    let roundResult = round;
+    displayScore(roundResult);
+    
+}
 
-// console.log(playRound(playerSelection, computerSelection));
-game();
+function displayScore(roundResult) {
+    if (roundResult.toLowerCase() == "computer"){
+        computerScore ++;
+        computerSDisplay.textContent = computerScore;
+        
+
+        
+    }
+    else if (roundResult.toLowerCase() == "user"){
+        playerScore ++;
+        playerSDisplay.textContent = playerScore;
+       
+        
+    }
+    if (playerScore==5 || computerScore == 5){
+        displayResults();
+    }
+    
+    console.log(`The player score is : ${playerScore}`);
+    console.log(`The pc score is : ${computerScore}`);
+}
+
+function displayResults() {
+    if (playerScore == 5) {
+        endDisplay.textContent = "Player wins !"
+        
+    }
+    else {
+        endDisplay.textContent = "Computer wins !"
+      
+    }
+
+}
+
+
+
+function reset() {
+
+    //reset all scores and scoreboards + messages 
+    // reset_btn.classList.add("fadeIn");
+    // reset_btn.disabled = false;
+
+    playerScore = 0;
+    playerSDisplay.textContent = 0;
+
+
+    computerScore = 0;
+    computerSDisplay.textContent = 0;
+    
+
+    endDisplay.textContent = ""
+
+
+    message.textContent = "";
+    
+}
+
+
+
+
+
